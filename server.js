64668067fc0584ae8bbf9ff69809401c33bca232
Michael Saly <michaelsaly@msn.com>
2018-11-24 23:33:58 +0000

    upd

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-    res.setheader("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Origin", "*")
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
     next()
