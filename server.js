30f7fdf8da13c8d91272d04595e1d7d25d861f7c
Michael Saly <michaelsaly@msn.com>
2018-11-24 22:10:59 +0000

    upd

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use('/requests', main)
 app.use(express.static(path.join(__dirname, 'dist')))
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'dist/index.html')) 
+    res.sendFile(path.join(__dirname, 'dist', 'index.html')) 
 })
 
 // Set port
